823b77702a1d953935227ca10e590c035068bc3a
Cgun-art | michael :) <migueloproio903@gmail.com>
2026-02-18 12:34:41 -0300

    Create doxygen.s.r

---
+++ settings/doxygen.s.r
@@ -0,0 +1,7 @@
+if DOXYGEN = true
+#endif <xboxone> = CREATE.DOXYGEN.PNG.WEBOS22.LATEST.0.7.RENDER.DXX.PNGDATA.RGB.A.TRUE.DOXYGEN=true.
+
+#console <xboxone>
+#meso <f32>
+#meso2 <xbox360>
+#chip <amd/gpu/add/cpu>
